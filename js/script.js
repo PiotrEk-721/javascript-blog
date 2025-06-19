cd71de860b56a929128a9218eed7404ecff487ce
Piotr Falkowski <piotrfalkowski721@gmail.com>
2025-06-20 01:18:31 +0200

    add function to remove 'active' atributes in article tags

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function titleClickHandler(event){
+  const clickedElement = this;
+  console.log('Link was clicked!');
+
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+
+    /* add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+
+    /* remove class 'active' from all articles */
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+}
