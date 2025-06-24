0d1f76c2d833f2d4259a4aefc4d5b0db057a296f
Piotr Falkowski <piotrfalkowski721@gmail.com>
2025-06-24 22:00:49 +0200

    fix problem with tag links

--- js/script.js
+++ js/script.js
@@ -35,6 +35,8 @@ const optArticleSelector = '.post';
 const optTitleSelector = '.post-title';
 const optTitleListSelector = '.titles';
 const optArticleTagsSelector = '.post-tags .list';
+const optTagSizeCount = 5;
+const optTagSizesClassPrefix = 'tag-size-';
 
 function generateTitleLinks() {
     
@@ -74,16 +76,41 @@ function generateTitleLinks() {
 
 generateTitleLinks();
 
+function calculateTagsOrAuthorsParams(parameters) {
+  const params = { max: 0, min: Infinity };
 
-function generateTags(){
+  for (let parameter in parameters) {
+    if (parameters[parameter] > params.max) {
+      params.max = parameters[parameter];
+    }
+    if (parameters[parameter] < params.min) {
+      params.min = parameters[parameter];
+    }
+  }
+  return params;
+}
+
+function calculateTagOrAuthorClass(count, params) {
+  const normalizedCount = count - params.min;
+  const normalizedMax = params.max - params.min;
+  const percentage = normalizedCount / normalizedMax;
+  const classNumber = Math.floor(percentage * (optTagSizeCount - 1) + 1);
+
+  return classNumber;
+}
+
+function generateTags() {
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
+
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
-
   /* START LOOP: for every article: */
   for (let article of articles) {
 
     /* find tags wrapper */
     const tagsWrappers = article.querySelector(optArticleTagsSelector);
+    console.log(tagsWrappers);
     tagsWrappers.innerHTML = '';
 
     /* make html variable with empty string */
@@ -96,25 +123,56 @@ function generateTags(){
     const articleTagsArray = articleTags.split(' ');
 
     /* START LOOP: for each tag */
-    for (let tags of articleTagsArray){
+    for (let tag of articleTagsArray) {
 
       /* generate HTML of the link */
       const linkHTML = `
       <li>
-        <a href="#tag-${tags}">${tags}</a>
+        <a href="#tag-${tag}">${tag}</a>
       </li>
       `;
 
       /* add generated code to html variable */
       html = html + linkHTML;
+      /* [NEW] check if this link is NOT already in allTags */
+      if (!allTags.hasOwnProperty(tag)) {
+        /* [NEW] add tag to allTags object */
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
+      }
     }
-
     /* END LOOP: for each tag */
 
     /* insert HTML of all the links into the tags wrapper */
     tagsWrappers.innerHTML = html;
 
-  /* END LOOP: for every article: */
+    /* END LOOP: for every article: */
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags.list');
+
+    /* [NEW] create variable for all links HTML code */
+    const tagsParams = calculateTagsOrAuthorsParams(allTags);
+    // console.log('tagsParams', tagsParams);
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for (let tag in allTags) {
+      /* [NEW] generate code of a link and add it to allTagHTML */
+      const tagLinkHTML = `
+        <li>
+          <a class="${optTagSizesClassPrefix}${calculateTagOrAuthorClass(allTags[tag], tagsParams)}" href="#${tag}">${tag}</a>
+          (${calculateTagOrAuthorClass(allTags[tag], tagsParams)})
+        </li>
+      `;
+
+      allTagsHTML += tagLinkHTML;
+    }
+
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /* [NEW] add html from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
   }
 }
 
